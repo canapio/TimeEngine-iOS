3f3146835509861c79ad206820bc64e780771c6c
canapio <canapio.developer@gmail.com>
2015-10-17 12:57:35 +0900

    Success time engine demo.

--- TimeListenerDEMO/TimeListenerDEMO/ViewController.m
+++ TimeListenerDEMO/TimeListenerDEMO/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "TimeEngine.h"
 
-@interface ViewController ()
+@interface ViewController () <TimeChangeDelegate>
 
 @end
 
@@ -17,11 +18,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [TimeEngine shared].changeType = tType_second;      // 초 단위로 호출      timeChanged:changeType: 호출
+    [TimeEngine shared].afterInterval = .1f;            // 시간이 바뀌기 직전에  timeWillChange:changeType:afterInterval: 를 호출
+    [TimeEngine shared].delegate = self;                // delegate를 설정
+    [[TimeEngine shared] MAKE_TIMER];                   // 타이머 시작
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+#pragma mark - Time Engine Delegate
+- (void) timeWillChange:(NSDate *)date changeType:(enum TimeChangeType)type afterInterval:(float)afterT {
+    NSLog(@"time will change after %f", afterT);
+}
+
+- (void) timeChanged:(NSDate *)date changeType:(enum TimeChangeType)type {
+    NSLog(@"current time is %@, change type : %zd", date, type);
 }
 
 @end
